10626d42bdacaff5602a64ade8eba469f093f7ba
sdenvs <sdenvs@gmail.com>
2021-12-07 14:01:48 +0530

    change-1

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,1 +1,58 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+
+class GoogleSuggestions extends Component {
+  state = {seach: ''}
+
+  changeSearch = event => {
+    this.setState({seach: event.target.value})
+  }
+
+  changeValue = value => {
+    this.setState({seach: value})
+  }
+
+  render() {
+    const {suggestionsList} = this.props
+    const {seach} = this.state
+    const showList = suggestionsList.filter(eachItem =>
+      eachItem.suggestion.toLowerCase().includes(seach.toLowerCase()),
+    )
+    return (
+      <div className="bgContainer">
+        <img
+          className="logo"
+          alt="google logo"
+          src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+        />
+        <div className="inputEl-container">
+          <img
+            className="searchIcon"
+            alt="search icon"
+            src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+          />
+          <input
+            onChange={this.changeSearch}
+            placeholder="Search Google"
+            className="imputEl"
+            type="search"
+            value={seach}
+          />
+        </div>
+        <ul className="list-container">
+          {showList.map(eachItem => (
+            <SuggestionItem
+              changeValue={this.changeValue}
+              key={eachItem.id}
+              details={eachItem}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,1 +1,26 @@
 // Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {details, changeValue} = props
+  const {suggestion} = details
+
+  const changeValueFun = () => {
+    changeValue(suggestion)
+  }
+
+  return (
+    <li className="suggetion-item">
+      <p>{suggestion}</p>
+      <img
+        onClick={changeValueFun}
+        value={suggestion}
+        className="arrowIcon"
+        alt="arrow"
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+      />
+    </li>
+  )
+}
+
+export default SuggestionItem
